b245e746aa0cb64111fa718354134d0c8830c70c
Eti Priya <etipriya@gmail.com>
2021-03-18 00:36:27 +0000

    add let command to array and add new array for character

--- script.js
+++ script.js
@@ -2,15 +2,20 @@
 var generateBtn = document.querySelector("#generate");
 
 // Declare character array in global memory
-
 lowerCaseArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-
 upperCaseArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-
 numericArray = ['0','1','2','3','4','5','6','7','8','9'];
-
 specialChar = ['!','@','$','%','&','+'];
-// Declare a new function on line 3
+
+let lowerCaseArray;
+let upperCaseArray;
+let numericArray;
+let specialChar;
+
+// Declare a new array to contain the character
+const chosenCharArray [];
+
+//
 function generatePassword() {
 	// Create a variable to store our generated password
 	var password = ''
