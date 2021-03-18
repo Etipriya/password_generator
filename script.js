db42a300964a332ac86de3ad50af52ed5ccb4515
Eti Priya <etipriya@gmail.com>
2021-03-18 17:04:18 +0000

    Added const generate password

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 var generateBtn = document.querySelector("#generate");
 
 // Declare character array in global memory
-lowerCaseArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-upperCaseArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-numberArray = ['0','1','2','3','4','5','6','7','8','9'];
-specialChar = ['!','@','$','%','&','+'];
+lowerCaseArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+upperCaseArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+numberArray = ['0','1','2','3','4','5','6','7','8','9']
+specialChar = ['!','@','$','%','&','+']
 
 let LowerCase;
 let UpperCase;
@@ -13,7 +13,7 @@ let NumberChar;
 let SpecialChar;
 
 // Declare a new array to contain the character
-const chosenCharArray = [];
+const chosenCharArray = []
 //console.log (chosenCharArray);
 
 // Password length using prompt and changing the string to number
@@ -42,6 +42,21 @@ if(passwordLengthNumber >= 8 && passwordLengthNumber <= 128){
   alert("Your password should contain characters from 8 to 128");
 }
 
+// Add validation function for character using chose
+if(isLowerCase){
+  chosenCharArray.push(lowerCaseArray);
+} else if (isUpperCase){
+  chosenCharArray.push(upperCaseArray);
+}else if (isNumberChar){
+  chosenCharArray.push(numberArray);
+}else if (isSpecialChar){
+  chosenCharArray.push(specialChar);
+}
+
+// Created generate password variable
+const generatePassword = function(){
+
+}
 function generatePassword() {
 	// Create a variable to store our generated password
 	var password = ''
