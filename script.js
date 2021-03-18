3b73abe505c9a49a5a2bd438bf6e43b1a3bd9d90
Eti Priya <etipriya@gmail.com>
2021-03-18 01:48:06 +0000

    Add Validation for the length of password

--- script.js
+++ script.js
@@ -4,28 +4,44 @@ var generateBtn = document.querySelector("#generate");
 // Declare character array in global memory
 lowerCaseArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 upperCaseArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-numericArray = ['0','1','2','3','4','5','6','7','8','9'];
+numberArray = ['0','1','2','3','4','5','6','7','8','9'];
 specialChar = ['!','@','$','%','&','+'];
 
-let lowerCaseArray;
-let upperCaseArray;
-let numericArray;
-let specialChar;
+let LowerCase;
+let UpperCase;
+let NumberChar;
+let SpecialChar;
 
 // Declare a new array to contain the character
 const chosenCharArray = [];
-console.log (chosenCharArray);
+//console.log (chosenCharArray);
 
 // Password length using prompt and changing the string to number
-const passwordNum = () => {
+const passwordNumber = () => {
   const passwordLength = prompt(
     "How long would you want your password to be?"
   );
   console.log(passwordLength);
-  const passwordLengthNum = parseInt(passwordLength,10);
-  console.log(passwordLengthNum);
+  const passwordLengthNumber = parseInt(passwordLength,10);
+  console.log(passwordLengthNumber);
 }
-//
+// Validation for length of the password 
+if(passwordLengthNumber >= 8 && passwordLengthNumber <= 128){
+  const isLowerCase = confirm("Does your Password includes lower characters?");
+  console.log(isLowerCase);
+
+  const isUpperCase = confirm("Does your Password includes upper characters?");
+  console.log(isUpperCase);
+
+  const isNumberChar = confirm("Does your Password includes numbers?");
+  console.log(isNumberChar);
+
+  const isSpecialChar = confirm("Does your Password includes special characters?");
+  console.log(isSpecialChar);
+}else{
+  alert("Your password should contain characters from 8 to 128");
+}
+
 function generatePassword() {
 	// Create a variable to store our generated password
 	var password = ''
